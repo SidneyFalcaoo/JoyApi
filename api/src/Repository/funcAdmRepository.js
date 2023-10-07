834e08d7b8470dea5186da128ccce622ea943dab
SidneyFalcaoo <sidneyoliveirafalcaooo@gmail.com>
2023-10-07 16:22:51 -0300

    OK

--- api/src/Repository/funcAdmRepository.js
+++ api/src/Repository/funcAdmRepository.js
@@ -24,19 +24,21 @@ export async function Logar(email, senha) {
 
 export async function AdicionarProduto(produto) {
     const comando = `
-            insert into tabela_produtos ( nome, preco, categoria_id, estoque, composicao, detalhes )
-                                 values ( ?, ?, ?, ?, ?, ?)
+        insert into tabela_produtos ( nome, preco, estoque, composicao, detalhes, categoria_id, subCategoria_id, imagem_produto_id )
+                             values ( ?, ?, ?, ?, ?, ?, ?, ?)
     `;
 
     const [resposta] = await conexao.query(comando, 
         [
             produto.nome,
             produto.preco,
-            produto.categoria,
             produto.estoque,
             produto.composicao,
-            produto.detalhes
-        ])      
+            produto.detalhes,
+            produto.categoria,
+            produto.SubCategoria,
+            produto.imagem_id
+        ]);      
 
         produto.id = resposta.insertId;
         return produto;
