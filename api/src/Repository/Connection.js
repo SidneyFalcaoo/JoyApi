463fdc3b42fe85ca78053badbd0826a73fb1be5d
Suyndara <souzamatheussilva41@gmail.com>
2023-10-27 15:15:07 -0300

    ok

--- api/src/Repository/Connection.js
+++ api/src/Repository/Connection.js
@@ -2,10 +2,10 @@ import mysql2 from 'mysql2/promise';
 
 
 const conexao = await mysql2.createConnection({
-	host: process.env.HOST,
-	database: process.env.DB,
-	user: process.env.USER,
-	password: process.env.PWD,
+	host: process.env.MYQSL_HOST,
+	database: process.env.MYQSL_DB,
+	user: process.env.MYQSL_USER,
+	password: process.env.MYQSL_PWD,
 	typeCast: function (field, next) {
 		if (field.type === "TINY" && field.length === 1) {
 			return (field.string() === '1');
