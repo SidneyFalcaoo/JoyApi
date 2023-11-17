003bee38702dd4daff769ce613c56b1e2d33d59c
Suyndara <souzamatheussilva41@gmail.com>
2023-11-17 14:16:37 -0300

    ok

--- api/src/Repository/funcAdmRepository.js
+++ api/src/Repository/funcAdmRepository.js
@@ -634,7 +634,7 @@ export async function BuscarAllUsers() {
                         cliente,
                         email,
                         senha,
-                        CadastroPessoa,	
+                        cpf,	
                         telefone,
                         nascimento
               from tabela_cliente`;
